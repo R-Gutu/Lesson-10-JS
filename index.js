a24ecbc26a8dd07f4561a91c44f9233bf1ccda86
Roman <r.gutsu@bk.ru>
2019-10-31 17:29:58 +0300

    home-work

---
+++ index.js
@@ -0,0 +1,16 @@
+var one= "Joe Doe"; /* string */
+var two=22; /* number */
+var three=true; /* boolean */
+var four; /* undefined */
+var five=null; /* null */
+var six ={ name : "roma" }; /* object */
+
+var MyCurrentAge=18 ;
+if (MyCurrentAge <= 25) {
+    console.log("I am under 25");
+}
+else {
+    console.log("I am over 25 years old");
+};
+
+MyCurrentAge<=25?(console.log("I am under 25") ) : (console.log("I am over 25 years old"));

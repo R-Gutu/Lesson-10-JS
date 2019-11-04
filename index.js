354a92c4a4c4c394f1313043cf7b46add38def32
Natalia <nezalzov_natalia@mail.ru>
2019-11-04 19:55:16 +0200

    fixed

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-var one= "Joe Doe"; /* string */
-var two=22; /* number */
-var three=true; /* boolean */
+var one = "Joe Doe"; /* string */
+var two =22; /* number */
+var three =true; /* boolean */
 var four; /* undefined */
-var five=null; /* null */
-var six ={ name : "roma" }; /* object */
+var five = null; /* null */
+var six = { name : "roma" }; /* object */
 
-var youngStr="I am under 25";
-var oldStr="I am over 25 years old";
-var MyCurrentAge=18 ;
+var youngStr = "I am under 25";
+var oldStr = "I am over 25 years old";
+var myCurrentAge = 18;
 var result;
-result= MyCurrentAge<=25?(result=youngStr):(result=oldStr);
+result = myCurrentAge <=25 ? (result=youngStr) : (result=oldStr);
 console.log(result);
 /*
-if (MyCurrentAge<=25){ result=youngStr;
+if (myCurrentAge <= 25){ result = youngStr;
 } else {
-    result=oldStr;
+    result = oldStr;
 }
-*/
 
-/*
-if (MyCurrentAge <= 25) {
+
+if (myCurrentAge <= 25) {
     console.log("I am under 25");
-}
-else {
+} else {
     console.log("I am over 25 years old");
 };
+
+MyCurrentAge <=25 ? (console.log("I am under 25") ) : (console.log("I am over 25 years old")); 
 */
-
-/*
-MyCurrentAge<=25?(console.log("I am under 25") ) : (console.log("I am over 25 years old")); */
-

d65b2a86616a72ba644c6b3e92efadce835c36f1
Natalia <nezalzov_natalia@mail.ru>
2019-11-04 19:39:53 +0200

    new

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ var four; /* undefined */
 var five=null; /* null */
 var six ={ name : "roma" }; /* object */
 
+var youngStr="I am under 25";
+var oldStr="I am over 25 years old";
 var MyCurrentAge=18 ;
+var result;
+result= MyCurrentAge<=25?(result=youngStr):(result=oldStr);
+console.log(result);
+/*
+if (MyCurrentAge<=25){ result=youngStr;
+} else {
+    result=oldStr;
+}
+*/
+
+/*
 if (MyCurrentAge <= 25) {
     console.log("I am under 25");
 }
 else {
     console.log("I am over 25 years old");
 };
+*/
 
-MyCurrentAge<=25?(console.log("I am under 25") ) : (console.log("I am over 25 years old"));
+/*
+MyCurrentAge<=25?(console.log("I am under 25") ) : (console.log("I am over 25 years old")); */
+
